4b23c9fe5b4cc075c81d2efa8c466ece872939d3
Mario Eduardo Garcia Gonzalez <pamario98@hotmail.com>
2021-02-14 18:34:53 -0600

    initial commit

---
+++ LOVE/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  LOVE
+//
+//  Created by Mario Eduardo García González on 22/11/19.
+//  Copyright © 2019 Mario Eduardo García González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
